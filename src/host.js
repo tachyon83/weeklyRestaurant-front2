733293b85b21c0bff4f6b380f4b5a1fa755c6182
mark-jaeho <carpe724@gmail.com>
2021-02-09 00:36:18 +0900

    중간 커밋

--- src/host.js
+++ src/host.js
@@ -1,3 +1,4 @@
 module.exports = {
-    server: (process.env.NODE_ENV==='production') ? null : "http://localhost:3002",
+    // server: (process.env.NODE_ENV==='production') ? null : "http://localhost:3002",
+    server: "https://weekly-restaurant-back.herokuapp.com",
 };  
