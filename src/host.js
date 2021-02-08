405fcdef548d43b2fca0fa6b34f10b507409cbaf
tachyon <83tachyon@gmail.com>
2021-02-08 23:05:08 +0900

    server host addr changed

--- src/host.js
+++ src/host.js
@@ -1,4 +1,4 @@
 module.exports = {
     // server: (process.env.NODE_ENV==='production') ? null : "http://localhost:3002",
-    server: "https://weekly-restaurant-back.herokuapp.com/",
+    server: "https://weekly-restaurant-back.herokuapp.com",
 };  
