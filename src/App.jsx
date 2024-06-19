f38b6d50c09c0619e873a431105d9a82962b1f72
tachyon <83tachyon@gmail.com>
2024-06-20 03:45:50 +0900

    [fix] Switch -> Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
-import Main from './component/Main';
-import Login from './component/Login';
-import Navigation from './component/Navigation';
-import Inventory from './component/Inventory';
-import CookingForm from './component/CookingForm';
-import CookingList from './component/CookingList';
-import CookingDetail from './component/CookingDetail';
-import Loading from './component/Loading';
+import React, { useState, useEffect } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  Redirect,
+} from "react-router-dom";
+import Main from "./component/Main";
+import Login from "./component/Login";
+import Navigation from "./component/Navigation";
+import Inventory from "./component/Inventory";
+import CookingForm from "./component/CookingForm";
+import CookingList from "./component/CookingList";
+import CookingDetail from "./component/CookingDetail";
+import Loading from "./component/Loading";
 
 const App = () => {
   const [islogin, setIslogin] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if(JSON.parse(sessionStorage.getItem('islogin'))){
-      setIslogin(true)
+    if (JSON.parse(sessionStorage.getItem("islogin"))) {
+      setIslogin(true);
     }
-  }, [])
+  }, []);
 
-  return(
-    <Router basename={'/'}>
+  return (
+    <Router basename={"/"}>
       <Loading isLoading={isLoading} />
       <Navigation islogin={islogin} setIslogin={setIslogin} />
       <main>
         <div className="layoutWrap">
-          <Switch>
+          <Routes>
             <Redirect from="/weeklyRestaurant" to="/" />
             <Route exact path="/">
-              <Main 
-                islogin={islogin}
-                setIsLoading={setIsLoading} 
-              />
+              <Main islogin={islogin} setIsLoading={setIsLoading} />
             </Route>
             <Route path="/login">
-              {islogin 
-                ? <Redirect to="/"/>
-                : <Login setIslogin={setIslogin} />
-              }
+              {islogin ? (
+                <Redirect to="/" />
+              ) : (
+                <Login setIslogin={setIslogin} />
+              )}
             </Route>
-            <Route 
-              path="/cookingList/:cookingId" 
-              render={()=><CookingDetail islogin={islogin} setIsLoading={setIsLoading} />}
+            <Route
+              path="/cookingList/:cookingId"
+              render={() => (
+                <CookingDetail islogin={islogin} setIsLoading={setIsLoading} />
+              )}
             />
             <Route path="/cookingList">
               <CookingList setIsLoading={setIsLoading} />
@@ -55,12 +60,14 @@ const App = () => {
             <Route path="/inventory">
               <Inventory setIsLoading={setIsLoading} />
             </Route>
-          </Switch>
+          </Routes>
         </div>
       </main>
-      <footer><div className="layoutWrap">Copyright &copy; 2020 - 2021</div></footer>
+      <footer>
+        <div className="layoutWrap">Copyright &copy; 2020 - 2021</div>
+      </footer>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
