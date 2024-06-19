3af5e608f1767b58438412bc44ceac13dad255fe
tachyon <83tachyon@gmail.com>
2024-06-19 23:17:55 +0900

    [fix] backend address fixed

--- src/host.js
+++ src/host.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // server: (process.env.NODE_ENV==='production') ? null : "http://localhost:3002",
-    server: "https://weekly-restaurant-back.herokuapp.com",
-};  
+  // server: (process.env.NODE_ENV==='production') ? null : "http://localhost:3002",
+  server: "https://weekly-restaurant-52f1377c9df8.herokuapp.com",
+};
